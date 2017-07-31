36dc6f1872dea25dfcd0155f57535467318b6873
darknath <nathan.preel@gmail.com>
2017-07-31 12:40:22 +0300

    update store module

--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -1,13 +1,36 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { StoreRoutingModule } from './store-routing.module';
+
+import { StoreService } from './services/store.service';
+import { CustomerListResolver } from './services/customer-list-resolver.service';
+import { ProductListResolver } from './services/product-list-resolver.service';
+
+import { OrderDetailsComponent } from './order-details/order-details.component';
+import { CustomersDropdownComponent } from './customers-dropdown/customers-dropdown.component';
+import { CustomerOrdersDropdownComponent } from './customer-orders-dropdown/customer-orders-dropdown.component';
+import { OrderItemsTableComponent } from './order-items-table/order-items-table.component';
+import { NewOrderComponent } from './new-order/new-order.component';
 
 @NgModule({
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     StoreRoutingModule
   ],
-  declarations: []
+  declarations: [
+    OrderDetailsComponent,
+    CustomersDropdownComponent, 
+    CustomerOrdersDropdownComponent, 
+    OrderItemsTableComponent, 
+    NewOrderComponent
+  ],
+  providers: [
+    StoreService,
+    CustomerListResolver,
+    ProductListResolver
+  ]
 })
 export class StoreModule { }
