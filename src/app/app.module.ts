143d2098b0f73045a57ef4557ea29992a9df1627
darknath <nathan.preel@gmail.com>
2017-07-31 12:40:54 +0300

    update app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { StoreModule } from './store/store.module';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
+    CommonModule,
+    HttpModule,
     AppRoutingModule,
     StoreModule
   ],
