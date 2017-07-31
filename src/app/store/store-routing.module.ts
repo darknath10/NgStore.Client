5deadd7cc5b4aed5efa8b10f91392e1556d8ec3e
darknath <nathan.preel@gmail.com>
2017-07-31 12:40:40 +0300

    update store routing module

--- src/app/store/store-routing.module.ts
+++ src/app/store/store-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { CustomerListResolver } from './services/customer-list-resolver.service';
+import { ProductListResolver } from './services/product-list-resolver.service';
+
+import { OrderDetailsComponent } from './order-details/order-details.component';
+import { NewOrderComponent } from './new-order/new-order.component';
+
+const routes: Routes = [
+  {
+    path: 'orders',
+    component: OrderDetailsComponent,
+    resolve: { customers: CustomerListResolver }
+  },
+  {
+    path: 'neworder',
+    component: NewOrderComponent,
+    resolve: { customers: CustomerListResolver, products:  ProductListResolver }
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
