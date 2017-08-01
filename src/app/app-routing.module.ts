0f0eee77c71a82651e28e2fd1e0212d7c04b509b
darknath <nathan.preel@gmail.com>
2017-08-01 20:35:43 +0300

    update app routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { WelcomeComponent } from './welcome/welcome.component';
+
 const routes: Routes = [
   {
+    path: 'welcome',
+    component: WelcomeComponent
+  },
+  {
     path: '',
-    children: []
+    redirectTo: 'welcome',
+    pathMatch: 'full'
   }
 ];
 
