548875e3f47b2ab26702c73d0a985b106b479922
darknath <nathan.preel@gmail.com>
2017-08-01 20:34:45 +0300

    update app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,23 +5,31 @@ import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { StoreModule } from './store/store.module';
+import { UserModule } from './user/user.module';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 
+import { TOASTR_PROVIDER } from './services/toastr.service';
+import { WelcomeComponent } from './welcome/welcome.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    WelcomeComponent,
   ],
   imports: [
     BrowserModule,
     CommonModule,
     HttpModule,
     AppRoutingModule,
-    StoreModule
+    StoreModule,
+    UserModule
   ],
-  providers: [],
+  providers: [
+    TOASTR_PROVIDER
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
